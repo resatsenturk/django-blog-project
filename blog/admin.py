d02c8d908313054d340ec81cdc61235e40f59f07
Reşat Şentürk <resatsenturk16@gmail.com>
2023-11-30 15:15:35 +0300

    soyut model eklendi ve admin.register decoretoru oluşturuldu

--- blog/admin.py
+++ blog/admin.py
@@ -1,23 +1,21 @@
 from django.contrib import admin
 from blog.models import KategoriModel,YazilarModel,YorumModel,IletisimModel
 
-admin.site.register(KategoriModel)
+admin.register(KategoriModel)
 
+@admin.register(YazilarModel)
 class YazilarAdmin(admin.ModelAdmin):
     search_fields=('baslik','icerik')
     list_display=(
         'baslik','olusturulma_tarihi','duzenlenme_tarihi'
     )
 
-admin.site.register(YazilarModel,YazilarAdmin)
-
+@admin.register(YorumModel)
 class YorumAdmin(admin.ModelAdmin):
-    list_display = ( 'yazan','olusturulma_tarihi','guncellenme_tarihi')
+    list_display = ( 'yazan','olusturulma_tarihi','duzenlenme_tarihi')
     search_fields = ('yazan__username',)
-admin.site.register(YorumModel,YorumAdmin)
 
+@admin.register(IletisimModel)
 class IletisimAdmin(admin.ModelAdmin):
     list_display=('email','olusturulma_tarihi')
     search_fields = ('email',)
-    
-admin.site.register(IletisimModel,IletisimAdmin)
