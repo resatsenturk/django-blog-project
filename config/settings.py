c6e26f2dfd6a6294179394855a4d766ab0217ea2
Reşat Şentürk <resatsenturk16@gmail.com>
2023-11-30 14:50:40 +0300

    account modeli ve admin ayarları tamamlandı

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'ckeditor'
+    'ckeditor',
+    #third party
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -108,3 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL='account.CustomUserModel'
