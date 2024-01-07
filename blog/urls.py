e226a25d37d45f82f8af86f5e93dec9634270a8a
Reşat Şentürk <resatsenturk16@gmail.com>
2024-01-07 18:45:11 +0300

    Proje tamamen bitti.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from blog.views import iletisim,anasayfa,kategori,yazilarim,detay,yazi_ekle,yazi_guncelle,yazi_sil,yorum_sil
-
+from django.views.generic import TemplateView,RedirectView
 
 
 urlpatterns = [
     path('',anasayfa,name='anasayfa'),
+    path('hakkimda',TemplateView.as_view(
+        template_name='pages/hakkimda.html'
+        ),name='hakkimda'),
+    path('yonlendir',RedirectView.as_view(
+        url='https://www.google.com'
+        ),name='iletisim'),
     path('iletisim',iletisim,name='iletisim'),
     path('kategori/<slug:kategoriSlug>',kategori,name='kategori'),
     path('yazilarim',yazilarim,name='yazilarim'),
